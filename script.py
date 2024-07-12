f22c759749c91d8215e026857eb6a02c248377a0
OliviaButters <111284147+OliviaButters@users.noreply.github.com>
2024-07-12 15:18:11 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,109 @@
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+import os
+import shutil
+from zipfile import ZipFile
+from glob import glob
+import subprocess
+
+# Define Data Paths
+data_path = os.getenv('DATA_PATH', '/data')
+inputs_path = os.path.join(data_path,'inputs')
+
+
+# Define Input Paths
+boundary_path = os.path.join(inputs_path,'boundary')
+vector_path = os.path.join(inputs_path, 'vectors')
+parameters_path = os.path.join(inputs_path, 'parameters')
+
+# Define and Create Output Paths
+outputs_path = os.path.join(data_path, 'outputs')
+outputs_path_ = data_path + '/' + 'outputs'
+if not os.path.exists(outputs_path):
+    os.mkdir(outputs_path_)
+greenareas_path = os.path.join(outputs_path, 'green_areas')
+greenareas_path_ = outputs_path + '/' + 'green_areas'
+if not os.path.exists(greenareas_path):
+    os.mkdir(greenareas_path_)
+
+# Look to see if a parameter file has been added
+parameter_file = glob(parameters_path + "/*.csv", recursive = True)
+print('parameter_file:', parameter_file)
+
+# Identify the EPSG projection code
+if len(parameter_file) == 1 :
+    parameters = pd.read_csv(parameter_file[0])
+    projection = parameters.loc[2][1]
+    print('projection:',projection)
+else:
+    projection = os.getenv('PROJECTION')
+
+# Identify input polygons and shapes (boundary of city, and OS grid cell references)
+boundary_1 = glob(boundary_path + "/*.*", recursive = True)
+print('Boundary File:',boundary_1)
+
+# Read in the boundary
+boundary = gpd.read_file(boundary_1[0])
+
+# Check boundary crs matches the projection
+if boundary.crs != projection:
+    boundary.to_crs(epsg=projection, inplace=True)
+
+print('boundary_crs:', boundary.crs)
+
+# Identify the name of the boundary file for the city name
+file_path = os.path.splitext(boundary_1[0])
+print('File_path:',file_path)
+filename=file_path[0].split("/")
+print('filename:',filename)
+location = filename[-1]
+print('Location:',location)
+
+# Identify if the buildings are saved in a zip file
+green_files_zip = glob(vector_path + "/*.zip", recursive = True)
+print(green_files_zip)
+
+# If yes, unzip the file (if the user has formatted the data correctly, this should reveal a .gpkg)
+if len(green_files_zip) != 0:
+    print('zip file found')
+    with ZipFile(green_files_zip[0],'r') as zip:
+        zip.extractall(vector_path)
+
+# Identify geopackages containing the polygons of the buildings
+green_files = glob(vector_path + "/*.gpkg", recursive = True)
+
+if len(green_files) != 0:
+    # Create a list of all of the gpkgs to be merged
+    to_merge=[]
+    to_merge=['XX' for n in range(len(green_files))]
+    for i in range (0,len(green_files)):
+        file_path = os.path.splitext(green_files[i])
+        filename=file_path[0].split("/")
+        to_merge[i]=filename[4]+'.gpkg'
+
+    print('to_merge:',to_merge)
+
+    # Create a geodatabase and merge the data from each gpkg together
+    all_green = []
+    all_green=gpd.GeoDataFrame(all_green)
+    for cell in to_merge:
+        gdf = gpd.read_file('/data/inputs/vectors/%s' %cell)
+        all_green = pd.concat([gdf, all_green],ignore_index=True)
+
+    all_green.to_crs(epsg=projection, inplace=True)
+
+    clipped = gpd.clip(all_green,boundary)
+
+    permeable_areas = 'polygons'
+
+    # Print to a gpkg file
+    clipped.to_file(os.path.join(greenareas_path, location + '.gpkg'),driver='GPKG',index=False)
+else:
+    permeable_areas = os.getenv('PERMEABLE_AREAS')
+
+print('permeable_areas:',permeable_areas)
+
+# Print all of the input parameters to an excel sheet to be read in later
+with open(os.path.join(parameter_file[0]), 'a') as f:
+    f.write('PERMEABLE_AREAS,%s\n' %permeable_areas)
